b947ce02901b83a49ace74e3e4786c8329e26ff7
Farsali <35901018+Edik1n0@users.noreply.github.com>
2021-05-26 12:35:05 -0500

    inicio db

--- database/control.sql
+++ database/control.sql
@@ -1,8 +1,8 @@
--- CREATE TABLE cusers(
+-- CREATE TABLE clients(
 -- 	id INT(11) NOT NULL,
---     cuser VARCHAR(16) NOT NULL,
---     cpass VARCHAR(60) NOT NULL,
---     cname VARCHAR(100) NOT NULL,
+--     nombre VARCHAR(100) NOT NULL,
+--     telefono VARCHAR(60) NOT NULL,
+--     email VARCHAR(60) NOT NULL,
 --     cphone INT(40) NOT NULL,
 --     cemail VARCHAR(60) NOT NULL
 -- );
--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
         host: 'us-cdbr-east-03.cleardb.com',
-        user: 'be7135c7eb02c0',
-        password: '57475ea8',
-        database: 'heroku_cea3c0fd4f250a2'
+        user: 'b99dd32b9293bc',
+        password: 'c502fb27',
+        database: 'heroku_412f3b1d483a5b8'
     }
 };
